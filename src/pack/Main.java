cdc1d9e4be00148540f4f7611789946e0fe39be6
tsma5611 <94079335+tsma5611@users.noreply.github.com>
2021-11-23 22:11:01 +0300

    tool out

--- src/pack/Main.java
+++ src/pack/Main.java
@@ -1,8 +1,22 @@
 package pack;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("start");
+        try {
+            Path path = Paths.get("file.txt");
+            Files.write(path, "Привет NIO".getBytes("cp1251"));
+            byte[] bytes = Files.readAllBytes(path);
+            System.out.println(new String(bytes, "cp1251"));
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
