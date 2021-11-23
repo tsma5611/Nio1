7411957bcd68be8b610ab773d94a73f914938c90
tsma5611 <94079335+tsma5611@users.noreply.github.com>
2021-11-23 22:11:44 +0300

    tool Files

--- src/pack/Main.java
+++ src/pack/Main.java
@@ -9,6 +9,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("start");
+
         try {
             Path path = Paths.get("file.txt");
             Files.write(path, "Привет NIO".getBytes("cp1251"));
